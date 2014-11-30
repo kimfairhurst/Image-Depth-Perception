c2d757448fd7fe658b39331ac45a07e39b0ce481
Kim Fairhurst <kimberlyfairhurst@gmail.com>
2014-11-30 14:21:48 -0800

    Create calcDepthNaive.h

---
+++ calcDepthNaive.h
@@ -0,0 +1,4 @@
+#pragma once
+
+float displacementNaive(int dx, int dy);
+void calcDepthNaive(float *depth, float *left, float *right, int imageWidth, int imageHeight, int featureWidth, int featureHeight, int maximumDisplacement, size_t* floatOps);
